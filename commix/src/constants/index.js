ba40fb47e293e02416db252b21c594ceb820ebcc
Kenneth Rebello <kennethrebello253@gmail.com>
2024-12-30 16:49:40 +0530

    edited navbar

--- commix/src/constants/index.js
+++ commix/src/constants/index.js
@@ -42,7 +42,7 @@ import {
 export const navigation = [
   {
     id: "0",
-    title: "Features",
+    title: "Our Services",
     url: "#features",
   },
   {
@@ -55,23 +55,23 @@ export const navigation = [
     title: "How to use",
     url: "#how-to-use",
   },
-  {
-    id: "3",
-    title: "Roadmap",
-    url: "#roadmap",
-  },
-  {
-    id: "4",
-    title: "New account",
-    url: "#signup",
-    onlyMobile: true,
-  },
-  {
-    id: "5",
-    title: "Sign in",
-    url: "#login",
-    onlyMobile: true,
-  },
+  // {
+  //   id: "3",
+  //   title: "Roadmap",
+  //   url: "#roadmap",
+  // },
+  // {
+  //   id: "4",
+  //   title: "New account",
+  //   url: "#signup",
+  //   onlyMobile: true,
+  // },
+  // {
+  //   id: "5",
+  //   title: "Sign in",
+  //   url: "#login",
+  //   onlyMobile: true,
+  // },
 ];
 
 export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
