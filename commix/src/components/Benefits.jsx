fa1915b99570b1bdf85c9add3fa1258e34c3cced
Kenneth Rebello <kennethrebello253@gmail.com>
2024-12-30 17:55:28 +0530

    updated and changed paths

--- commix/src/components/Benefits.jsx
+++ commix/src/components/Benefits.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import { benefits } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
-import Arrow from "../assets/svg/Arrow";
+import Arrow from "/public/assets/svg/Arrow";
 import { GradientLight } from "./design/Benefits";
-import ClipPath from "../assets/svg/ClipPath";
+import ClipPath from "/public/assets/svg/ClipPath";
 
 const Benefits = () => {
   const [showAll, setShowAll] = useState(false);
